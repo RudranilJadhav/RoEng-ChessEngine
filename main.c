13e3e08519414e2484fcdd7a735443b7cbf3e3e4
Rudranil Jadhav <45rudraniljadhav@gmail.com>
2025-11-01 04:11:00 +0530

    Added Move-Gen

--- main.c
+++ main.c
@@ -10,6 +10,11 @@ int main() {
     const char* fen="rnbqkbnr/pppppppp/8/8/4P3/5N2/PPPP1PPP/RNBQKB1R b KQkq - 1 2";
     fen_to_position(fen);
     print_position(&pos);
+    int move=MakeMove(e7,e5,DoublePawn,0);
+    make_move(move);
+    print_position(&pos);
+    undo_move();
+    print_position(&pos);
 
     return 0;
 }
---
+++ move_gen.h
@@ -0,0 +1,11 @@
+#ifndef MOVE_GEN_H
+#define MOVE_GEN_H
+
+
+int genrate_moves(Move *moves_list);
+int genrate_pawn_moves(Move *move_list);
+int genrate_sliding_moves(Move *movelist);
+int genrate_knight_moves(Move *move_list)
+
+
+#endif
